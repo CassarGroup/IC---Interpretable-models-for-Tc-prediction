857b1af8e69fa1a9468d446e4ee21709806969b6
JuliaGuedesASantos <julia24002@ilum.cnpem.br>
2025-07-03 13:03:19 -0300

    update

--- script_glm.py
+++ script_glm.py
@@ -1,8 +1,14 @@
 import numpy as np
-from sklearn.base import BaseEstimator, ClassifierMixin, clone
+import statistics as st
+from sklearn.base import BaseEstimator, RegressorMixin, clone
 from ucimlrepo import fetch_ucirepo 
+import statsmodels.api as sm
+from sklearn.model_selection import KFold
+from sklearn.metrics import root_mean_squared_error
 
-def load_dataset(): # How to import the dataset with UCI repository
+random_seed = 1203
+
+def load_dataset(): # Import the dataset from UCI repository
     # Import dataset
     superconductivty_data = fetch_ucirepo(id=464) 
   
@@ -12,38 +18,93 @@ def load_dataset(): # How to import the dataset with UCI repository
 
     return X, y
 
-class PerClusterClassifier(BaseEstimator, ClassifierMixin):
-    def __init__(self, clusterer, base_estimator):
+class PerClusterClassifier(BaseEstimator, RegressorMixin):
+    def __init__(self, clusterer):
         self.clusterer = clusterer
-        self.base_estimator = base_estimator # Base class for all the estimators
 
-    def fit(self, X, y):
+    def fit(self, X, y, distribution):
+        self.X = X
+        self.y = y
         # Fit the clustering algorithm
         self.clusterer_ = clone(self.clusterer) # Construct a new unfitted estimator with the same parameters for each entrance 
-        cluster_labels = self.clusterer_.fit_predict(X) # Use the fit/predict method
+        cluster_labels = self.clusterer_.fit_predict(X) # Use the fit/predict method for data clustering 
         self.cluster_labels_ = cluster_labels
         self.models_ = {}
-        
+        self.distribution = distribution 
         # For each cluster, fit a supervised model
-        for cluster in np.unique(cluster_labels):
+        for cluster in np.unique(cluster_labels): # identify the classes
             idx = np.where(cluster_labels == cluster)[0]
-            X_cluster = X[idx]
-            y_cluster = y[idx]
-            model = clone(self.base_estimator)
-            model.fit(X_cluster, y_cluster)
-            self.models_[cluster] = model
+            X_cluster = X.iloc[idx]
+            y_cluster = y.iloc[idx]
+            
+            # link_function = sm.families.links.Log()
+
+            # distribution = sm.families.Gamma(link=link_function)
+            #X_cluster = sm.add_constant(X_cluster, has_constant='add')
+
+            model_glm = sm.GLM(y_cluster, X_cluster, family=distribution)
+
+            glm = model_glm.fit()
+
+            self.models_[cluster] = glm
         return self
 
     def predict(self, X):
+        # Assign clusters to new data
+        #X = sm.add_constant(X, has_constant='add')
         cluster_labels = self.clusterer_.predict(X)
-        y_pred = np.empty(X.shape[0], dtype=object)
+        y_pred = np.empty(X.shape[0])
         for cluster in np.unique(cluster_labels):
             idx = np.where(cluster_labels == cluster)[0]
             if cluster in self.models_:
-                y_pred[idx] = self.models_[cluster].predict(X[idx])
+                y_pred[idx] = self.models_[cluster].predict(X.iloc[idx])
             else:
-                # Handle unseen clusters (optional: fallback strategy)
-                y_pred[idx] = None
+                y_pred[idx] = np.nan
         return y_pred
+    
+    def rmse(self, X, y):
+        y_pred = list(self.predict(X))
+        y_true = list(y.values)
+        rmse = 0
+        for t, p in zip(y_true, y_pred):
+            rmse += (t-p) ** 2
+        rmse = (rmse/ len(y)) ** (1/2)
+        return rmse
+    
+    def cross_validation(self, n_splits=5):
+        cluster_labels = self.clusterer_.predict(self.X) # predict only the label of the entrance
+        all_rmse = {}
+
+        for cluster in np.unique(cluster_labels):
+            # Cross validation for each cluster
+            idx = np.where(cluster_labels == cluster)[0]
+            X_cluster = self.X.iloc[idx]
+            #X_cluster = sm.add_constant(X_cluster, has_constant='add')
+            y_cluster = self.y.iloc[idx]
+
+            kf = KFold(n_splits=n_splits, shuffle=True, random_state=random_seed)
+            cluster_rmse = [] # RMSE only for the cluster
+
+            for train_idx, test_idx in kf.split(X_cluster):
+                X_train, X_test = X_cluster.iloc[train_idx], X_cluster.iloc[test_idx]
+                y_train, y_test = y_cluster.iloc[train_idx], y_cluster.iloc[test_idx]
+                
+                # Training a new GLM model for each cluster
+                model_glm = sm.GLM(y_train, X_train, family=self.distribution)
+                glm = model_glm.fit()
+
+                # Make the prediction
+                y_pred = glm.predict(X_test)
+
+                # Calculate RMSE
+                rmse = root_mean_squared_error(y_test.values, y_pred)
+                cluster_rmse.append(rmse)
+
+            all_rmse[cluster] = np.mean(cluster_rmse)
+        cluster_values = list(all_rmse.values())
+        all_rmse["Mean"] = st.mean(cluster_values)
+        all_rmse["Median"] = st.median(cluster_values)
+        
+        return all_rmse
 
         
